1001e9a78133025f7ee250f37334dfcefb63e843
bharatpatil <patilbharat09@gmail.com>
2019-07-03 19:05:04 +0530

    upadate password

--- credentials.js
+++ credentials.js
@@ -1,7 +1,7 @@
 module.exports = {
     mongo: {
       development: {
-        connectionString: 'mongodb://graphUser:gslab@123@ds345937.mlab.com:45937/mygraph'
+        connectionString: 'mongodb://graphUser:gslab123@ds345937.mlab.com:45937/mygraph'
       }
     }
   }
